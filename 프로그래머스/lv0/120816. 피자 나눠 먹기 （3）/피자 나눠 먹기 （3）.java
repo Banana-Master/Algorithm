22d0ceefdbf0b1faaa4365da0f76efc448a85c32
Sejuani98 <120389368+Sejuani98@users.noreply.github.com>
2023-01-01 20:59:22 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int slice, int n) {
+        if (n % slice == 0) {
+            return n / slice;
+        } else if (n < slice) {
+            return 1;
+        } else {
+            return n / slice + 1;
+        }
+        
+    }
+}
