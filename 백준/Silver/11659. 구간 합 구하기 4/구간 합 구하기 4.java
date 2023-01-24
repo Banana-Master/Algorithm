e7767b339bcf5d8c03c0cc13bfd4f6454bfe4e17
김성호 <120389368+Sejuani98@users.noreply.github.com>
2023-01-24 15:18:33 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1488 ms, Memory: 58984 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,31 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] array = new int[sc.nextInt()+1];
-        int num = sc.nextInt();
-        for (int i = 1; i < array.length; i++) {
-            int num2 = sc.nextInt();
-            array[i] = array[i -1] + num2;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+        long[] S = new long[suNo +1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i -1] + Integer.parseInt(stringTokenizer.nextToken());
         }
-        int max, min = 0;
-        int[] answer = new int[num];
-        for (int i = 0; i < answer.length; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            if(a > b) {
-                max = a;
-                min = b;
-            } else {
-                max = b;
-                min = a;
-            }
-            answer[i] = array[max] - array[min -1];
-        }
-        for (int i = 0; i < answer.length; i++) {
-            System.out.println(answer[i]);
-
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i -1]);
         }
     }
 }
