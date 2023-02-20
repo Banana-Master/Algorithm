b42bf5cd052699d9ca85d1af510ad06e850307ab
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-02-20 10:54:55 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int num = num2 - num1 + 1;
+        int[] answer = new int[num];
+        for(int i = num1, j=0; i <= num2; i++,j++) {
+            answer[j] = numbers[i];
+        }
+        return answer;
+    }
+}
