d0a5922835b7215d30068db243e81c5ada80e8bb
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-03-21 08:47:25 +0900

    [level 0] Title: 로그인 성공?, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,16 @@
+function solution(id_pw, db) {
+    let result = 'fail'
+    for(let i = 0; i < db.length; i++) {
+        if(id_pw[0] == db[i][0]) {
+            if(id_pw[1] == db[i][1]) {
+                result = 'login'
+            } else {
+                result = 'wrong pw'
+            }
+            break;
+        }
+    }
+    
+    
+    return result;
+}
