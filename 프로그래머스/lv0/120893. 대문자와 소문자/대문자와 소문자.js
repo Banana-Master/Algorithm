34e4ceefbe363ca3f99766a086a01d6daddf2f39
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-03-26 19:58:16 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -3,7 +3,8 @@ function solution(my_string) {
     let result = ''
     for(let i = 0; i < my_string.length; i++) {
         let c = my_string[i].charCodeAt()
-        result += c >= 97 ? String.fromCharCode(c - 32) : String.fromCharCode(c + 32)
+        // result += c >= 97 ? String.fromCharCode(c - 32) : String.fromCharCode(c + 32)
+        result += c < 97 ? my_string[i].toLowerCase() : my_string[i].toUpperCase()
     }
     return result;
 }
