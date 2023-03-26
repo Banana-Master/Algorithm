4d638526fbcba01563ec04848eabe206f1777557
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-03-26 11:30:09 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,8 +1,7 @@
 function solution(s) {
     let en = ['zero','one','two','three','four','five','six','seven','eight','nine']
-    let num = ['0','1','2','3','4','5','6','7','8','9']
     for(let i = 0; i < en.length; i++) {
-        s = s.replaceAll(en[i],num[i])
+        s = s.replaceAll(en[i],i)
     }
     return parseInt(s);
 }
