dbaf34f0d49041a54c9a74076d75140ff5605d75
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-03-27 10:36:17 +0900

    [level 0] Title: 모스부호 (1), Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    let result = ''
+    let answer = letter.split(' ');
+    for(let i = 0; i < answer.length; i++) {
+        result += answer[i].replace(answer[i],morse[answer[i]])
+    }
+    return result;
+}
