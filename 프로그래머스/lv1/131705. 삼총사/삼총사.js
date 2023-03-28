263e99121bf56f1600ad7d8b55baa4e090e5a5cc
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-03-28 09:02:01 +0900

    [level 1] Title: 삼총사, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,13 @@
+function solution(number) {
+    let result = 0
+    for(let i = 0; i < number.length; i++) {
+        for(let j = 0; j < number.length; j++) {
+            for(let k = 0; k < number.length; k++) {
+                if(i != j && j != k && k != i && (number[i] + number[j] + number[k]) == 0) {
+                    result++
+                }
+            }
+        }
+    }
+    return result / 6;
+}
