2fc341a1a74de6b1df5c92da8d9fb4647aa97229
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-03-29 09:10:10 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,13 +1,4 @@
 function solution(array) {
-    let count = 0
-    for(let i = 0; i < array.length; i++) {
-        array[i] += ''
-        let a = array[i].split('')
-        for(let j = 0; j < a.length; j++) {
-            if(a[j] === '7') {
-                count++
-            }
-        }
-    }
-    return count;
+    
+    return array.join('').split('7').length -1;
 }
