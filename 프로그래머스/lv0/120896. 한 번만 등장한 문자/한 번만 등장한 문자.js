29be818d223261e0b06b98b6297d7694b240e933
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-04-04 09:02:08 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,7 @@
+function solution(s) {
+    let result = ''
+    for(let i = 0; i < s.length; i++) {
+        result += s.split(s[i]).length == 2 ? s[i] : ''
+    }
+    return [...result].sort().join('');
+}
