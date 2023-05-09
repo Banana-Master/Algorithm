69c0e4a7b37d406f4b6073e7debb7b3fda82906f
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-05-09 09:01:31 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,10 @@
+function solution(a, b, c) {
+    if(a != b && b != c && c != a) {
+        return a + b + c;
+    } else if(a == b && b == c && c == a) {
+        return (a + b + c) * (a * a + b * b + c * c) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+    } else {
+        return (a + b + c) * (a * a + b * b + c * c);
+    }
+    
+}
