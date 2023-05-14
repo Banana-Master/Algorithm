f3254c7718b6c34fe138dfcb33d67aa713c1bf56
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-05-15 08:53:33 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 152 ms, Memory: 9492 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.js
@@ -0,0 +1,35 @@
+/* readline Module */
+
+// 문제 풀이
+function solution(input) {
+
+	// 답변 출력, list
+	return (input[0] + input[1]).toString();
+   
+}
+
+/* readline Module */
+const readline = require("readline");
+const rl = readline.createInterface({
+
+    input: process.stdin,
+    output: process.stdout,
+    
+});
+
+let input;
+let list = [];
+
+rl.on("line", function (line) {
+
+    input = line; // 입력받은 문자열, line
+    rl.close();   // 입력 종료
+    
+}).on("close", function () {
+
+    // 형변환, 구분자(띄어쓰기)기준으로 배열에 삽입
+    list = input.split(' ').map((el) => BigInt(el));
+
+    console.log(solution(list)); // 문제 풀이 함수 호출
+    process.exit(); // 프로세스 종료
+});
