356a1dd44911af5e520b61e6e39c54b6a2c02799
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-13 02:55:37 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,15 +1,15 @@
 function solution(k, score) {
     let result = [score[0]];
-    let the_Hall_of_Fame = [score[0]];
+    let 명예의전당 = [score[0]];
     for(let i = 1; i < score.length; i++) {
-        if(Math.min(...the_Hall_of_Fame) <= score[i] || the_Hall_of_Fame.length !== k) {
-            the_Hall_of_Fame.push(score[i]);
+        if(명예의전당[명예의전당.length -1] <= score[i] || 명예의전당.length !== k) {
+            명예의전당.push(score[i]);
         }
-        the_Hall_of_Fame.sort((x, y) => y - x);
-        if(the_Hall_of_Fame.length > k) {
-            the_Hall_of_Fame.pop()
+        명예의전당.sort((x, y) => y - x);
+        if(명예의전당.length > k) {
+            명예의전당.pop()
         }
-        result.push(Math.min(...the_Hall_of_Fame));
+        result.push(명예의전당[명예의전당.length -1]);
     }
     return result;
 }
