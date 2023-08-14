1ffbde2a64c5f0f0185a8e3d1deef3643cbd979c
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-14 18:56:11 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,8 +1,4 @@
 function solution(absolutes, signs) {
-    let sum = 0
-    for(let i = 0; i < signs.length; i++) {
-        signs[i] ? sum += absolutes[i] : sum -= absolutes[i]
-            
-    }
-    return sum;
+    let num = absolutes.map((x, i) => x *= signs[i] ? 1 : -1);
+    return num.reduce((x, y) => x + y);
 }
