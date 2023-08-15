0556063d5eab8d03a6385615e8c6f2eb832e8499
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-15 19:35:26 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,6 @@
+function solution(my_string, s, e) {
+    let front = my_string.slice(0, s);
+    let back = my_string.slice(e + 1);
+    let mid = [...my_string.slice(s, e + 1)].reverse().join('');
+    return front + mid + back;
+}
