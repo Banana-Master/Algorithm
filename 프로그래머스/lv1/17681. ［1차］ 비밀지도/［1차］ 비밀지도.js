d99926b99c8b51789cf09a291df7f6be0f61b4e7
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-17 19:40:02 +0900

    Update ［1차］ 비밀지도.js

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,28 +1,8 @@
 function solution(n, arr1, arr2) {
     let result = [];
-    let arr = Array.from({ length : n }).fill(' ');
-    let one = arr1.map((x) => {
-        let str = x.toString(2);
-        if(str.length === n) {
-            return str;
-        } else {
-            while(str.length !== n) {
-                str = '0' + str;
-            }
-            return str;
-        }
-    });
-    let two = arr2.map((x) => {
-        let str = x.toString(2);
-        if(str.length === n) {
-            return str;
-        } else {
-            while(str.length !== n) {
-                str = '0' + str;
-            }
-            return str;
-        }
-    });
+    let one = compare(arr1, n);
+    let two = compare(arr2, n);
+
     for(let i = 0; i < n; i++) {
         result[i] = [];
         for(let j = 0; j < n; j++) {
@@ -35,4 +15,18 @@ function solution(n, arr1, arr2) {
         result[i] = result[i].join('')
     }
     return result
+}
+
+function compare(arr, n) {
+    return arr.map((x) => {
+        let str = x.toString(2);
+        if(str.length === n) {
+            return str;
+        } else {
+            while(str.length !== n) {
+                str = '0' + str;
+            }
+            return str;
+        }
+    });
 }
