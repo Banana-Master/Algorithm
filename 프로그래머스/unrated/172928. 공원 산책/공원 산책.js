cf5b243f0d8e6271c8b0d4b4f3af1871ba40fbf8
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-19 21:29:05 +0900

    Update 공원 산책.js

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -1,7 +1,6 @@
 function solution(park, routes) {
-    let x = 0; // 로봇 강아지의 세로 방향 좌표
-    let y = 0; // 로봇 강아지의 가로 방향 좌표
-    // 공원에서 로봇 강아지의 시작 위치를 찾습니다.
+    let x = 0;
+    let y = 0; 
     for (let i = 0; i < park.length; i++) {
         for (let j = 0; j < park[i].length; j++) {
             if (park[i][j] === 'S') {
@@ -11,63 +10,58 @@ function solution(park, routes) {
             }
         }
     }
-    // 각 경로에 대해 반복합니다.
     for (let i = 0; i < routes.length; i++) {
         let route = routes[i].split(' ');
-        let direction = route[0]; // 이동할 방향
-        let distance = parseInt(route[1]); // 이동할 거리
+        let 방향 = route[0]; // 이동할 방향
+        let 거리 = parseInt(route[1]); // 이동할 거리
         let newX = x; // 새로운 세로 방향 좌표
         let newY = y; // 새로운 가로 방향 좌표
         // 이동할 방향에 따라 새로운 좌표를 계산합니다.
-        if (direction === 'N') {
-            newX -= distance;
-        } else if (direction === 'S') {
-            newX += distance;
-        } else if (direction === 'W') {
-            newY -= distance;
-        } else if (direction === 'E') {
-            newY += distance;
+        if (방향 === 'N') {
+            newX -= 거리;
+        } else if (방향 === 'S') {
+            newX += 거리;
+        } else if (방향 === 'W') {
+            newY -= 거리;
+        } else if (방향 === 'E') {
+            newY += 거리;
         }
-        // 새로운 좌표가 공원 내부에 있는지 확인합니다.
         if (newX >= 0 && newX < park.length && newY >= 0 && newY < park[0].length) {
-            let blocked = false; // 장애물이 있는지 여부
-            // 이동하는 경로에 장애물이 있는지 확인합니다.
-            if (direction === 'N') {
-                for (let j = x - 1; j >= newX; j--) {
+            let 장애물 = false;
+            if (방향 === 'N') {
+                for (let j = x; j >= newX; j--) {
                     if (park[j][y] === 'X') {
-                        blocked = true;
+                        장애물 = true;
                         break;
                     }
                 }
-            } else if (direction === 'S') {
-                for (let j = x + 1; j <= newX; j++) {
+            } else if (방향 === 'S') {
+                for (let j = x; j <= newX; j++) {
                     if (park[j][y] === 'X') {
-                        blocked = true;
+                        장애물 = true;
                         break;
                     }
                 }
-            } else if (direction === 'W') {
-                for (let j = y - 1; j >= newY; j--) {
+            } else if (방향 === 'W') {
+                for (let j = y; j >= newY; j--) {
                     if (park[x][j] === 'X') {
-                        blocked = true;
+                        장애물 = true;
                         break;
                     }
                 }
-            } else if (direction === 'E') {
-                for (let j = y + 1; j <= newY; j++) {
+            } else if (방향 === 'E') {
+                for (let j = y; j <= newY; j++) {
                     if (park[x][j] === 'X') {
-                        blocked = true;
+                        장애물 = true;
                         break;
                     }
                 }
             }
-            // 장애물이 없다면 로봇 강아지를 새로운 위치로 이동시킵니다.
-            if (!blocked) {
+            if (!장애물) {
                 x = newX;
                 y = newY;
             }
         }
     }
-    // 최종 위치를 반환합니다.
     return [x, y];
 }
