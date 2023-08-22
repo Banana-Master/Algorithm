df5511b69349191ae667c2280fe5ea106c1a3835
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-08-22 20:42:02 +0900

    [Silver IV] Title: 숫자놀이, Time: 212 ms, Memory: 12028 KB -BaekjoonHub

---
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.js
@@ -0,0 +1,52 @@
+const solution = (input) => {
+    const number = ['zero', 'one', 'two',
+        'three', 'four', 'five',
+        'six', 'seven', 'eight',
+        'nine'];
+    const english_number = new Map();
+    for (let i = input[0]; i <= input[1]; i++) {
+        let 십의자리 = Math.floor(i / 10);
+        let 일의자리 = i % 10;
+        const arr = [];
+        if (십의자리 !== 0) {
+            arr.push(number[십의자리]);
+        }
+        arr.push(number[일의자리]);
+        english_number.set(i, arr)
+    }
+    let sort = [...english_number.values()].sort((x, y) => x > y ? 1 : -1);
+    let result = [];
+    for (let i = 0; i < sort.length; i++) {
+        for (const [key, value] of english_number.entries()) {
+            if(JSON.stringify(value) === JSON.stringify(sort[i])) {
+                result.push(key);
+                continue;
+            }
+        }
+    }
+    let str = '';
+    let count = 0;
+    for(let i = 0; i < result.length; i++) {
+        count++;
+        str += result[i] + ' ';
+        if(count === 10) {
+            str += '\n'
+            count = 0;
+        }
+    }
+    return str;
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let input;
+rl.on("line", function (line) {
+    input = line.split(' ').map(Number);
+    rl.close();
+}).on("close", function () {
+    console.log(solution(input));
+    process.exit();
+});
