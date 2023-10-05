67ecf4824062b6d94e3888785e7fdbaf441d3f4e
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-10-05 09:54:18 +0900

    [level 2] Title: 삼각 달팽이, Time: 86.96 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    const arr = Array.from({length : n}, (_, i) => new Array(i + 1).fill(0));
+    let count = 0;
+    let x = -1;
+    let y = 0;
+    while(n > 0) {
+        for(let i = 0; i < n; i++) {
+            arr[++x][y] = ++count;
+        }
+        for(let i = 0; i < n - 1; i++) {
+            arr[x][++y] = ++count;
+        }
+        for(let i = 0; i < n - 2; i++) {
+            arr[--x][--y] = ++count;
+        }
+        n -= 3;
+    }
+    return arr.flatMap((x) => x)
+}
