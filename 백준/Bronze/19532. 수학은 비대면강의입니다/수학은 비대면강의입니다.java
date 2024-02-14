5c4c9328fb0c819945ecb7cddc7a1b17ccd9e522
김성호 <120389368+Banana-Master@users.noreply.github.com>
2024-02-14 19:24:23 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 156 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = reader.readLine().split(" ");
+        int a = Integer.parseInt(inputs[0]);
+        int b = Integer.parseInt(inputs[1]);
+        int c = Integer.parseInt(inputs[2]);
+        int d = Integer.parseInt(inputs[3]);
+        int e = Integer.parseInt(inputs[4]);
+        int f = Integer.parseInt(inputs[5]);
+        int x = (c * e - b * f) / (a * e - b * d);
+        int y = (c * d - a * f) / (b * d - a * e);
+
+        System.out.println(x + " " + y);
+    }
+}
