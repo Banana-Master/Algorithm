8148d500581bfafd9a071b80ad3e5e2e1a838294
김성호 <120389368+Banana-Master@users.noreply.github.com>
2024-07-29 10:59:24 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon {
+    class Program {
+        static void Main(string[] args) {
+            string[] input = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(input[0]) + int.Parse(input[1]));
+        }
+    }
+}
